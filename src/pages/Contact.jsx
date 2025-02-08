4befdfe4d53abf40a30349ef4ab720eb7e2953e3
remiborris <remiborris@gmail.com>
2025-02-07 20:42:44 -0400

    added logos to contact page

--- src/pages/Contact.jsx
+++ src/pages/Contact.jsx
@@ -1,4 +1,6 @@
 import React from "react";
+import { FaGithub, FaLinkedin, FaPhoneAlt } from "react-icons/fa";
+import { MdEmail } from "react-icons/md";
 
 const Contact = () => {
 
@@ -6,30 +8,40 @@ const Contact = () => {
     <div className="max-w-lg mx-auto bg-white shadow-lg rounded-2x1 p-6 mt-8">
       <h2 className="text-2x1 font-bold text-gray-900 mb-4">Contact</h2>
       <ul className="space-y-2 text-gray-700">
-        <li>
-          <span className="font-semibold">Email: RemiBorris@gmail.com</span>
+        <li className="flex items-center space-x-2">
+          <MdEmail className="text-lg"/> 
+          <span className="font-semibold">Email:</span>{" "}
+          <a 
+            href="mailto:remi.borris@gmail.com"
+            className="text-blue-500 hover:underline"
+          >
+            RemiBorris@gmail.com 
+          </a>
         </li>
-        <li>
+        <li className="flex items-center space-x-2">
+          <FaPhoneAlt className="text-lg"/>
           <span className="font-semibold">Phone: 506-380-1101</span>
         </li>
-        <li>
+        <li className="flex items-center space-x-2">
+          <FaLinkedin className="text-lg"/>
           <span className="font-semibold">LinkedIn:</span>{" "}
           <a
             href="https://linkedin.com/in/RemiBorris"
             target="_blank"
             rel="noopener noreferrer"
-            className="text-blue-500 hover: underline"
+            className="text-blue-500 hover:underline"
           >
             linkedin.com/in/RemiBorris
           </a>
         </li>
-        <li>
+        <li className="flex items-center space-x-2">
+          <FaGithub className="text-lg"/>
           <span className="font-semibold">GitHub:</span>{" "}
           <a
             href="https://github.com/RemiBorris"
             target="_blank"
             rel="noopener noreferrer"
-            className="text-blue-500 hover: underline"
+            className="text-blue-500 hover:underline"
           >
           github.com/RemiBorris
           </a>
