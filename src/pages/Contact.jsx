048717366a6b1e14720adf14c71237812af0c796
remiborris <remiborris@gmail.com>
2025-02-09 14:45:13 -0400

    changed linkedin icon to different icon as initial one was not appearing on mobile

--- src/pages/Contact.jsx
+++ src/pages/Contact.jsx
@@ -1,5 +1,6 @@
 import React from "react";
-import { FaGithub, FaLinkedin, FaPhoneAlt } from "react-icons/fa";
+import { FaGithub, FaPhoneAlt } from "react-icons/fa";
+import { FaLinkedin } from "react-icons/fa6";
 import { MdEmail } from "react-icons/md";
 
 const Contact = () => {
