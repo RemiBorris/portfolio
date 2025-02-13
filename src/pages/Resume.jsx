19968485b53bf105cf04b34d8ad390c6e6995761
remiborris <remiborris@gmail.com>
2025-02-12 20:43:31 -0400

    changed the resume link to get taken from google drive, not be uploaded directly

--- src/pages/Resume.jsx
+++ src/pages/Resume.jsx
@@ -1,43 +1,23 @@
-import React, { useState } from "react";
-import { Document, Page, pdfjs } from "react-pdf";
+import React from "react";
 import Button from "../components/Button";
 
-pdfjs.GlobalWorkerOptions.workerSrc = `https://cdnjs.cloudflare.com/ajax/libs/pdf.js/${pdfjs.version}/pdf.worker.min.js`;
+const resumeID = "1445dR0_J_1Ue9Is2QzAyyawuySC_o1hR";
+const resumePreviewURL = `https://drive.google.com/file/d/${resumeID}/preview`;
+const resumeDownloadURL = `https://drive.google.com/uc?export=download&id=${resumeID}`;
 
 const Resume = () => {
-  const [numPages, setNumPages] = useState(null);
-
-  const onDocumentLoadSuccess = ({ numPages }) => {
-    setNumPages(numPages);
-  };
 
   return (
     <div className="flex flex-col items-center justify-center p-4 pt-6">
       <h2 className="text-2xl font-bold mb-4 text-center">My Resume</h2>
 
-      <div className="hidden md:flex w-full max-w-screen-md md:justify-center overflow-auto">
-        <Document
-          file="/Remi-Borris-Resume.pdf"
-          onLoadSuccess={onDocumentLoadSuccess}
-          className="flex flex-col items-center"
-        >
-          {numPages &&
-            Array.from(new Array(numPages), (el, index) => (
-              <Page 
-                key={`page_${index + 1}`} 
-                pageNumber={index + 1} 
-                renderTextLayer={false} 
-                renderAnnotationLayer={false} 
-                className="w-full max-w-full h-auto"
-                scale={1.0}
-              />
-            ))
-          }
-        </Document>
-      </div>
+      <iframe
+        src={(resumePreviewURL)}
+        className="hidden md:flex w-full max-w-screen-lg h-[600px] border border-gray-300"
+      ></iframe>
 
       {/* Download Button */}
-      <a href="/Remi-Borris-Resume.pdf" download className="mt-4">
+      <a href={resumeDownloadURL} download className="mt-4">
         <Button text="Download Resume" />
       </a>
     </div>
