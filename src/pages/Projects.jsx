724567041b2626700cf2839cde7c072de0e5066e
remiborris <remiborris@gmail.com>
2025-02-21 19:14:52 -0400

    changed project videos to gifs, added additional projects and changed layout of tiles for desktop view

--- src/pages/Projects.jsx
+++ src/pages/Projects.jsx
@@ -1,14 +1,15 @@
 import React, { useEffect } from "react";
 import { useLocation } from "react-router-dom";
 
-const ProjectCard = ({ title, videoSrc, techStack, repoLink, id }) => {
+const ProjectCard = ({ title, videoSrc, gifSrc, techStack, repoLink, id }) => {
   return (
     <div className="bg-white shadow-lg rounded-2xl p-6 mb-6" id={id}>
       <h3 className="text-xl font-bold mb-2">{title}</h3>
-      <video controls className="w-full rounded-lg mb-3">
+      {/* <video controls className="w-full rounded-lg mb-3">
         <source src={videoSrc} type="video/mp4" />
         Your browser does not support the video tag.
-      </video>
+      </video> */}
+      <img src={gifSrc} alt={title} className="w-full rounded-lg mb-3" />
       <p className="text-gray-700"><strong>Tech Stack:</strong> {techStack}</p>
       <a
         href={repoLink}
@@ -36,35 +37,63 @@ const Projects = () => {
 
   return (
     <div className="max-w-4xl mx-auto p-6">
+      {/* <img src="/videos/flexForge.gif" alt="Flex Forge" /> */}
       <h2 className="text-2xl font-bold mb-4 text-center">Projects</h2>
-      <ProjectCard
-        title="Flex Forge"
-        videoSrc="/videos/flexForge.mp4"
-        techStack="React, Node.js, Axios, pg, Ruby on Rails"
-        repoLink="https://github.com/RemiBorris/flex-forge"
-        id="flexforge"
-      />
-      <ProjectCard
-        title="SmartToDo"
-        videoSrc="/videos/smartToDo.mp4"
-        techStack="JavaScript, Axios, bcrypt, pg, dotenv, ejs"
-        repoLink="https://github.com/RemiBorris/Smart_TODO"
-        id="smarttodo"
-      />
-      <ProjectCard
-        title="Photolabs"
-        videoSrc="/videos/photolabs.mp4"
-        techStack="React, Express, ReactDOM, pg"
-        repoLink="https://github.com/RemiBorris/photolabs"
-        id="photolabs"
-      />
-      <ProjectCard
-        title="Tweeter"
-        videoSrc="/videos/tweeter.mp4"
-        techStack="HTML, CSS, JavaScript, jQuery, Node.js, Express"
-        repoLink="https://github.com/RemiBorris/tweeter"
-        id="tweeter"
-      />
+      <div className="lg:grid lg:grid-cols-2 lg:gap-10">
+        <ProjectCard
+          title="Flex Forge"
+          // videoSrc="/videos/flexForge.mp4"
+          gifSrc="/videos/flexForge.gif"
+          techStack="React, Node.js, Axios, PostgreSQL, Ruby on Rails"
+          repoLink="https://github.com/RemiBorris/flex-forge"
+          id="flexforge"
+        />
+        <ProjectCard
+          title="SmartToDo"
+          // videoSrc="/videos/smartToDo.mp4"
+          gifSrc="/videos/smartToDo.gif"
+          techStack="JavaScript, Axios, bcrypt, PostgreSQL, dotenv, ejs"
+          repoLink="https://github.com/RemiBorris/Smart_TODO"
+          id="smarttodo"
+        />
+        <ProjectCard
+          title="Photolabs"
+          // videoSrc="/videos/photolabs.mp4"
+          gifSrc="/videos/photolabs.gif"
+          techStack="React, Express, ReactDOM, PostgreSQL"
+          repoLink="https://github.com/RemiBorris/photolabs"
+          id="photolabs"
+        />
+        <ProjectCard
+          title="Tweeter"
+          // videoSrc="/videos/tweeter.mp4"
+          gifSrc="/videos/tweeter.gif"
+          techStack="HTML, CSS, JavaScript, jQuery, Node.js, Express"
+          repoLink="https://github.com/RemiBorris/tweeter"
+          id="tweeter"
+        />
+        <ProjectCard
+          title="TinyApp"
+          gifSrc="/videos/tinyApp.gif"
+          techStack="Node.js, Express, EJS, bcrypt, cookie-session, method-override"
+          repoLink="https://github.com/RemiBorris/tinyapp"
+          id="tinyapp"
+        />
+        <ProjectCard
+          title="Jungle"
+          gifSrc={"/videos/jungle.gif"}
+          techStack="Rails, Bootstrap, PostgreSQL, Stripe"
+          repoLink="https://github.com/RemiBorris/jungle-rails"
+          id="jungle"
+        />
+        <ProjectCard
+          title="Scheduler"
+          gifSrc={"/videos/scheduler.gif"}
+          techStack="Axios, Classnames, Normalize.css, React, React-dom, React-scripts"
+          repoLink="https://github.com/RemiBorris/scheduler"
+          id="scheduler"
+        />
+      </div>
     </div>
   );
 };
